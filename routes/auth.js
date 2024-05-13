eaeb16dc3d4410a745ea3938ed8811ccdd368192
devansh0331 <devanshshrivastava07@gmail.com>
2024-05-14 02:07:08 +0530

    is-user

--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,12 @@ const router = express.Router();
 router.post("/register", register);
 // router.post("/login", verifyToken, login);
 router.post("/login", login);
+router.get("/is-user", verifyToken, async (req, res) => {
+  try {
+    res.status(200).json(true);
+  } catch (error) {
+    res.status(400).json(false);
+  }
+});
 
 export default router;
