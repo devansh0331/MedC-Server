e5a59901287509ba20de68ae68d8dd2a5dc06096
devansh0331 <devanshshrivastava07@gmail.com>
2024-11-11 01:57:38 +0530

    Inifinite Scroll

--- controllers/newposts.js
+++ controllers/newposts.js
@@ -1,17 +1,18 @@
 import Post from "../models/Post.js";
 export const newPost = async (req, res) => {
+  console.log("offset");
   const limit = 10; // Number of posts per page
   const offset = req.query.page ? (parseInt(req.query.page) - 1) * limit : 0;
-
   try {
     const paginatedPosts = await Post.find()
       .sort({ createdAt: -1 })
       .skip(offset)
-      .limit(limit);
+      .limit(limit)
+      .populate("user", "name profileURL bio");
 
     res.json(paginatedPosts);
   } catch (error) {
     console.error("Error fetching posts:", error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json("Internal server error");
   }
 };
